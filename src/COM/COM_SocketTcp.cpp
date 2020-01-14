acee5af8262fedead32e08a3a0e68d8ebb1da43b
Thomas CASPAR <thomas.caspar@outlook.com>
2020-01-14 23:32:00 +0100

    [init] init

---
+++ src/COM/COM_SocketTcp.cpp
@@ -0,0 +1,10 @@
+/*
+ * COM_SocketTcp.cpp
+ *
+ *  Created on: 14 janv. 2020
+ *      Author: thomas
+ */
+
+
+
+
---
+++ src/COM/COM_SocketTcp.h
@@ -0,0 +1,15 @@
+/*
+ * COM_SocketTcp.h
+ *
+ *  Created on: 14 janv. 2020
+ *      Author: thomas
+ */
+
+#ifndef SRC_COM_COM_SOCKETTCP_H_
+#define SRC_COM_COM_SOCKETTCP_H_
+
+
+
+
+
+#endif /* SRC_COM_COM_SOCKETTCP_H_ */
