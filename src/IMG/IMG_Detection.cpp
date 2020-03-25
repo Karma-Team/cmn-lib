6b06d0146fff87e8391083d9561a4839d2a1ceb7
Alexandre HUBERT <alexandre_hubert@hotmail.fr>
2020-03-25 22:31:08 +0100

    IMG (detection) :: Add the cupsDetection method

--- src/IMG/IMG_Detection.cpp
+++ src/IMG/IMG_Detection.cpp
@@ -10,10 +10,74 @@
 
 IMG::CDetection::CDetection()
 {
+	m_detectedRobotsNb 	= 0;
 }
 
 
 
 IMG::CDetection::~CDetection()
+{
+}
+
+
+
+void IMG::CDetection::cupsDetection(Mat& imgInImgCamera)
+{
+	Mat imgInImgCameraHsv;
+	Mat imgThresholdedGreen;
+	Mat imgThresholdedRed1;
+	Mat imgThresholdedRed2;
+	Mat imgThresholded;
+
+	// Convert a video frame from BGR to HSV
+		cvtColor(imgInImgCamera, imgInImgCameraHsv, COLOR_BGR2HSV);
+
+	// Threshold the image : inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded)
+		inRange(imgInImgCameraHsv, Scalar(HL_GREEN, 	SL_GREEN, 	VL_GREEN),	Scalar(HH_GREEN, 	SH_GREEN, 	VH_GREEN),	imgThresholdedGreen);
+		inRange(imgInImgCameraHsv, Scalar(HL_RED1, 	SL_RED1, 	VL_RED1),	Scalar(HH_RED1, 	SH_RED1, 	VH_RED1),	imgThresholdedRed1);
+		inRange(imgInImgCameraHsv, Scalar(HL_RED2, 	SL_RED2, 	VL_RED2),	Scalar(HH_RED2, 	SH_RED2, 	VH_RED2),	imgThresholdedRed2);
+		imgThresholded 	= imgThresholdedGreen + imgThresholdedRed1 + imgThresholdedRed2;
+
+	// Morphological opening (remove small objects from the foreground)
+		erode(imgThresholded,	imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(imgThresholded, 	imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+	// Morphological closing (fill small holes in the foreground)
+		dilate(imgThresholded, 	imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(imgThresholded, 	imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+	// Save the output image
+		m_outImgCupsDetection = imgThresholded.clone();
+
+	// FOR DEBUG ONLY (begin)
+	// Show the original and the thresholded image
+		imshow("Original Image", 		imgInImgCamera);
+		imshow("Original Image HSV",	imgInImgCameraHsv);
+		imshow("Thresholded Image",		imgThresholded);
+
+		// Wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		if(waitKey(1) == 27)
+		{
+			cout << "esc key is pressed by user" << endl;
+			return;
+		}
+	// FOR DEBUG ONLY (end)
+}
+
+
+
+void IMG::CDetection::arucoCodesDetection()
+{
+}
+
+
+
+void IMG::CDetection::robotsDetection()
+{
+}
+
+
+
+void IMG::CDetection::mergeDetections()
 {
 }
--- src/IMG/IMG_Detection.hpp
+++ src/IMG/IMG_Detection.hpp
@@ -13,10 +13,6 @@
 
 
 
-using namespace std;
-
-
-
 namespace IMG
 {
 	/**
@@ -35,6 +31,54 @@ namespace IMG
 				@brief destructor
 			 **/
 			virtual ~CDetection();
+
+			/**
+				@brief method for cups detection
+			 **/
+			void cupsDetection(Mat& inImgCamera);
+
+			/**
+				@brief method for aruco codes detection
+			 **/
+			void arucoCodesDetection();
+
+			/**
+				@brief method for robots detection
+			 **/
+			void robotsDetection();
+
+			/**
+				@brief method for merging detections
+			 **/
+			void mergeDetections();
+
+			/**
+				@brief method for getting the IN image (from camera)
+			 **/
+			void getInImgCamera();
+
+			/**
+				@brief method for getting the OUT image (from cups detection thread)
+			 **/
+			void getOutImgCupsDetection();
+
+			/**
+				@brief method for getting the OUT image (from robots detection thread)
+			 **/
+			void getOutImgRobotsDetection();
+
+			/**
+				@brief method for getting the OUT image (from merge detections)
+			 **/
+			void getOutImgMergeDetections();
+
+		private:
+			Mat 			m_inImgCamera;							//< IN image (from camera)
+			Mat 			m_tmpInImgCameraHsv;					//< IN image (from camera, converted in HSV)
+			Mat 			m_outImgCupsDetection;					//< OUT image (from cups detector thread)
+			Mat 			m_outImgRobotsDetection[NB_ROBOTS_MAX];	//< OUT image (from robots detector thread)
+			Mat 			m_outImgMergeDectections;				//< OUT image (from merge detections)
+			uint32_t		m_detectedRobotsNb;						//< Detected robots number
 	};
 }
 
